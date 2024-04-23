fe3e0497a70b8898ddaa1f6d77b50bd82afa86e8
JULIA PEREIRA SILVA <23111810@CFP127ALUNO.LOCAL>
2024-04-23 12:28:45 -0300

    terminando

---
+++ js/home.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const imagePath = '../img/fundo.png';
+
+        // Selecionar o elemento de fundo
+        const backgroundElement = document.getElementById('background');
+
+            // Calcular a altura desejada (por exemplo, metade da largura da tela)
+            const windowHeight = window.innerHeight;
+            const windowWidth = window.innerWidth;
+    
+
+        // Definir estilos CSS para o elemento de fundo
+        backgroundElement.style.backgroundImage = `url('${imagePath}')`;
+        backgroundElement.style.backgroundRepeat = 'no-repeat';
+        backgroundElement.style.backgroundSize = 'cover';
+        backgroundElement.style.backgroundPosition = 'center';
+        backgroundElement.style.opacity = 1.7;
+     
+
+const areaElement = document.getElementById('area');
+
+// Definir o estilo de gradiente linear
+areaElement.style.backgroundImage = 'linear-gradient(to bottom, #D9F9FE, #214D87)'; 
+
+const area2Element = document.getElementById('area2');
+// Definir o estilo de gradiente linear
+area2Element.style.backgroundImage = 'linear-gradient(to bottom, #D9F9FE, #214D87)'; 
